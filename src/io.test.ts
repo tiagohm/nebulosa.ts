2d8b09b61154d55b819cb9737ac3c55f834b00e5
tiagohm <tiago.henrique.cco@gmail.com>
2025-01-11 14:42:41 -0300

    implement io

--- src/io.test.ts
+++ src/io.test.ts
@@ -1,5 +1,8 @@
 import { expect, test } from 'bun:test'
-import { arrayBufferToLines, readableStreamToLines } from './io'
+import fs from 'fs/promises'
+import { tmpdir } from 'os'
+import { join } from 'path'
+import { arrayBufferToLines, bufferSink, bufferSource, fileHandleSink, fileHandleSource, readableStreamToLines } from './io'
 
 test('arrayBufferToLines', async () => {
 	const blob = new Blob(['line 1\n', 'line 2\n', '', 'line 3\nline 4\n\n'])
@@ -23,4 +26,162 @@ test('readableStreamToLines', async () => {
 
 	expect(lines).toHaveLength(4)
 	expect(lines).toContainAllValues(['line 1', 'line 2', 'line 3', 'line 4'])
+})
+
+test('bufferSink', () => {
+	const buffer = Buffer.alloc(16)
+	const sink = bufferSink(buffer)
+
+	expect(sink.write('abcdefg')).toBe(7)
+	expect(sink.write('h')).toBe(1)
+	expect(sink.write('ijklmnop')).toBe(8)
+
+	expect(buffer.toString()).toBe('abcdefghijklmnop')
+	expect(sink.exhausted).toBeTrue()
+
+	sink.seek(4)
+	expect(sink.write('abcd')).toBe(4)
+
+	expect(buffer.toString()).toBe('abcdabcdijklmnop')
+	expect(sink.exhausted).toBeFalse()
+
+	sink.seek(-4)
+	expect(sink.write('abcd')).toBe(4)
+
+	expect(buffer.toString()).toBe('abcdabcdijklabcd')
+	expect(sink.exhausted).toBeTrue()
+
+	sink.seek(0)
+	expect(sink.write('abcd', 1)).toBe(3)
+
+	expect(buffer.toString()).toBe('bcddabcdijklabcd')
+	expect(sink.exhausted).toBeFalse()
+
+	sink.seek(0)
+	expect(sink.write('abcd', 2, 2)).toBe(2)
+
+	expect(buffer.toString()).toBe('cdddabcdijklabcd')
+	expect(sink.exhausted).toBeFalse()
+
+	sink.seek(0)
+	expect(sink.write(Buffer.from('xyz'))).toBe(3)
+
+	expect(buffer.toString()).toBe('xyzdabcdijklabcd')
+	expect(sink.exhausted).toBeFalse()
+
+	expect(sink.write(Buffer.from('xyz'), 1)).toBe(2)
+
+	expect(buffer.toString()).toBe('xyzyzbcdijklabcd')
+	expect(sink.exhausted).toBeFalse()
+
+	expect(sink.write(Buffer.from('xyz'), 1, 1)).toBe(1)
+
+	expect(buffer.toString()).toBe('xyzyzycdijklabcd')
+	expect(sink.exhausted).toBeFalse()
+})
+
+test('fileHandleSink', async () => {
+	const path = join(tmpdir(), 'fhs.tmp')
+	const handle = await fs.open(path, 'w+', 0o666)
+	const sink = fileHandleSink(handle)
+
+	async function read() {
+		const buffer = Buffer.alloc(16)
+		const ret = await handle.read(buffer, 0, buffer.length, 0)
+		return ret.buffer.toString()
+	}
+
+	expect(await sink.write('abcdefg')).toBe(7)
+	expect(await sink.write('h')).toBe(1)
+	expect(await sink.write('ijklmnop')).toBe(8)
+
+	expect(await read()).toBe('abcdefghijklmnop')
+
+	sink.seek(4)
+	expect(await sink.write('abcd')).toBe(4)
+
+	expect(await read()).toBe('abcdabcdijklmnop')
+
+	sink.seek(0)
+	expect(await sink.write('abcd', 1)).toBe(3)
+
+	expect(await read()).toBe('bcddabcdijklmnop')
+
+	sink.seek(0)
+	expect(await sink.write('abcd', 2, 2)).toBe(2)
+
+	expect(await read()).toBe('cdddabcdijklmnop')
+
+	sink.seek(0)
+	expect(await sink.write(Buffer.from('xyz'))).toBe(3)
+
+	expect(await read()).toBe('xyzdabcdijklmnop')
+
+	expect(await sink.write(Buffer.from('xyz'), 1)).toBe(2)
+
+	expect(await read()).toBe('xyzyzbcdijklmnop')
+
+	expect(await sink.write(Buffer.from('xyz'), 1, 1)).toBe(1)
+
+	expect(await read()).toBe('xyzyzycdijklmnop')
+
+	await fs.unlink(path)
+})
+
+test('bufferSource', () => {
+	const buffer = Buffer.alloc(17, 32)
+	const source = bufferSource(Buffer.from('abcdefghijklmnop'))
+
+	expect(source.read(buffer)).toBe(16)
+
+	expect(buffer.toString()).toBe('abcdefghijklmnop ')
+	expect(source.exhausted).toBeTrue()
+
+	buffer.fill(32)
+	source.seek(0)
+	expect(source.read(buffer, 1)).toBe(16)
+
+	expect(buffer.toString()).toBe(' abcdefghijklmnop')
+	expect(source.exhausted).toBeTrue()
+
+	buffer.fill(32)
+	source.seek(0)
+	expect(source.read(buffer, 1, 6)).toBe(6)
+
+	expect(buffer.toString()).toBe(' abcdef          ')
+	expect(source.exhausted).toBeFalse()
+
+	expect(source.read(buffer, undefined, 1)).toBe(1)
+
+	expect(buffer.toString()).toBe('gabcdef          ')
+	expect(source.exhausted).toBeFalse()
+})
+
+test('fileHandleSource', async () => {
+	const path = join(tmpdir(), 'fhs.tmp')
+	const handle = await fs.open(path, 'w+', 0o666)
+	const source = fileHandleSource(handle)
+	const buffer = Buffer.alloc(17)
+
+	await handle.write('abcdefghijklmnop ', 0, 'ascii')
+
+	expect(await source.read(buffer)).toBe(17)
+
+	expect(buffer.toString()).toBe('abcdefghijklmnop ')
+
+	buffer.fill(32)
+	source.seek(0)
+	expect(await source.read(buffer, 1)).toBe(16)
+
+	expect(buffer.toString()).toBe(' abcdefghijklmnop')
+
+	buffer.fill(32)
+	source.seek(0)
+	expect(await source.read(buffer, 1, 6)).toBe(6)
+
+	expect(buffer.toString()).toBe(' abcdef          ')
+
+	expect(await source.read(buffer, undefined, 1)).toBe(1)
+
+	expect(buffer.toString()).toBe('gabcdef          ')
 })
--- src/io.ts
+++ src/io.ts
@@ -1,3 +1,5 @@
+import type { FileHandle } from 'fs/promises'
+
 export async function* arrayBufferToLines(buffer: AllowSharedBufferSource, charset: string = 'utf-8') {
 	const decoder = new TextDecoder(charset)
 	const stream = new ReadableStream<AllowSharedBufferSource>({
@@ -64,4 +66,141 @@ export async function* readableStreamToLines(stream: ReadableStream<Uint8Array>,
 	if (line) {
 		yield line
 	}
+}
+
+export interface Seekable {
+	readonly position: number
+	readonly exhausted: boolean
+
+	seek(position: number): boolean
+}
+
+export function isSeekable(o: object): o is Seekable {
+	return 'position' in o
+}
+
+export interface Sink {
+	write(chunk: string | Buffer, offset?: number, size?: number, encoding?: BufferEncoding): Promise<number> | number
+}
+
+export class BufferSink implements Sink, Seekable {
+	position = 0
+
+	constructor(private readonly buffer: Buffer) {}
+
+	get exhausted() {
+		return this.position >= this.buffer.length
+	}
+
+	seek(position: number): boolean {
+		const length = this.buffer.byteLength
+		if (position >= length || position <= -length) return false
+		if (position >= 0) this.position = position
+		else this.position = length + position
+		return true
+	}
+
+	write(chunk: string | Buffer, offset?: number, size?: number, encoding?: BufferEncoding) {
+		offset ??= 0
+
+		if (typeof chunk === 'string')
+			if (size === 0) return 0
+			else if (size === undefined && !offset) size = this.buffer.write(chunk, this.position, encoding)
+			else if (size === undefined) size = this.buffer.write(chunk.substring(offset), this.position, encoding)
+			else if (!offset) size = this.buffer.write(chunk.substring(0, size), this.position, encoding)
+			else size = this.buffer.write(chunk.substring(offset, offset + size), this.position, encoding)
+		else size = chunk.copy(this.buffer, this.position, offset, offset + (size ?? chunk.byteLength))
+		this.position += size
+		return size
+	}
+}
+
+export function bufferSink(buffer: Buffer): Sink & Seekable {
+	return new BufferSink(buffer)
+}
+
+export class FileHandleSink implements Sink, Seekable {
+	position = 0
+	readonly exhausted = false
+
+	constructor(private readonly handle: FileHandle) {}
+
+	seek(position: number) {
+		if (position < 0) return false
+		this.position = position
+		return true
+	}
+
+	async write(chunk: string | Buffer, offset?: number, size?: number, encoding?: BufferEncoding) {
+		if (typeof chunk === 'string')
+			if (size === 0) return 0
+			else if (size === undefined && !offset) size = (await this.handle.write(chunk, this.position, encoding)).bytesWritten
+			else if (size === undefined) size = (await this.handle.write(chunk.substring(offset!), this.position, encoding)).bytesWritten
+			else if (!offset) size = (await this.handle.write(chunk.substring(0, size), this.position, encoding)).bytesWritten
+			else size = (await this.handle.write(chunk.substring(offset, offset + size), this.position, encoding)).bytesWritten
+		else size = (await this.handle.write(chunk, offset, size, this.position)).bytesWritten
+		this.position += size
+		return size
+	}
+}
+
+export function fileHandleSink(handle: FileHandle): Sink & Seekable {
+	return new FileHandleSink(handle)
+}
+
+export interface Source {
+	read(buffer: Buffer, offset?: number, size?: number): Promise<number> | number
+}
+
+export class BufferSource implements Source, Seekable {
+	position = 0
+
+	constructor(private readonly buffer: Buffer) {}
+
+	get exhausted() {
+		return this.position >= this.buffer.length
+	}
+
+	seek(position: number): boolean {
+		const length = this.buffer.byteLength
+		if (position >= length || position <= -length) return false
+		if (position >= 0) this.position = position
+		else this.position = length + position
+		return true
+	}
+
+	read(buffer: Buffer, offset?: number, size?: number) {
+		size = Math.min(size ?? buffer.byteLength - (offset ?? 0), this.buffer.byteLength - this.position)
+		if (!size) return 0
+		size = this.buffer.copy(buffer, offset, this.position, this.position + size)
+		this.position += size
+		return size
+	}
+}
+
+export function bufferSource(buffer: Buffer): Source & Seekable {
+	return new BufferSource(buffer)
+}
+
+export class FileHandleSource implements Source, Seekable {
+	position = 0
+	exhausted = false
+
+	constructor(private readonly handle: FileHandle) {}
+
+	seek(position: number) {
+		if (position < 0) return false
+		this.position = position
+		return true
+	}
+
+	async read(buffer: Buffer, offset?: number, size?: number) {
+		const ret = await this.handle.read(buffer, offset ?? 0, size, this.position)
+		this.position += ret.bytesRead
+		return ret.bytesRead
+	}
+}
+
+export function fileHandleSource(handle: FileHandle): Source & Seekable {
+	return new FileHandleSource(handle)
 }
