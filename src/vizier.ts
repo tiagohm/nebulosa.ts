72a053df55350655c47ed6573e4f6ad097aa3b45
tiagohm <tiago.henrique.cco@gmail.com>
2025-02-22 07:53:15 -0300

    implement vizier

---
+++ src/vizier.ts
@@ -0,0 +1,20 @@
+import { TSV_DELIMITER, readCsv } from './csv'
+
+export const VIZIER_URL = 'http://tapvizier.cds.unistra.fr/'
+
+const VIZIER_QUERY_PATH = 'TAPVizieR/tap/sync'
+
+export async function vizierQuery(query: string, baseUrl?: string) {
+	const uri = `${baseUrl || VIZIER_URL}${VIZIER_QUERY_PATH}`
+
+	const body = new FormData()
+	body.append('request', 'doQuery')
+	body.append('lang', 'adql')
+	body.append('format', 'tsv')
+	body.append('query', query)
+
+	const response = await fetch(uri, { method: 'POST', body })
+	if (response.status >= 300) return undefined
+	const text = await response.text()
+	return readCsv(text, TSV_DELIMITER)
+}
