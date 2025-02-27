422877994b276cd5507e09ed4198baaae3d89653
tiagohm <tiago.henrique.cco@gmail.com>
2025-02-26 21:17:46 -0300

    fix csv

--- src/csv.test.ts
+++ src/csv.test.ts
@@ -1,29 +1,16 @@
 import { expect, test } from 'bun:test'
-import { parseCsvLine, readCsv } from './csv'
+import { readCsv } from './csv'
 
 test('csv', async () => {
 	const file = Bun.file('data/ephemeris.csv')
 	const csv = readCsv(await file.text())!
 
-	expect(csv.header).toHaveLength(17)
-	expect(csv.header).toEqual(['Year', 'Month', 'Day', 'Time', 'Age of Moon', 'RA h', 'RA m', 'Ra s', 'DEC deg', 'DEC m', 'DEC s', 'Rise', 'Culm', 'Set', 'Approx', 'Observable', 'Constellation'])
-	expect(csv.data).toHaveLength(25)
-	expect(csv.data[0]).toHaveLength(17)
-	expect(csv.data[0]).toEqual(['2025', 'Feb', '21', '21:00 GMT-03', '24 days', '17', '03', '29', '-27', '59', '46', '23:26', '06:24', '13:25', '-11.6', '00:05 until 05:36', 'Ophiuchus'])
-	expect(csv.data[24]).toEqual(['2025', 'Mar', '17', '21:00 GMT-03', '18 days', '14', '16', '54', '-17', '21', '51', '19:34', '02:01', '08:35', '-12.4', '20:09 until 05:47', 'Virgo'])
-})
+	const [header, ...data] = csv
 
-test('parseCsvLine', () => {
-	expect(parseCsvLine('a,b,c')).toEqual(['a', 'b', 'c'])
-	expect(parseCsvLine('"a",b,"c"')).toEqual(['a', 'b', 'c'])
-	expect(parseCsvLine('"a",b,"c,d"')).toEqual(['a', 'b', 'c,d'])
-	expect(parseCsvLine('"a,b","c,d","e,f"')).toEqual(['a,b', 'c,d', 'e,f'])
-	expect(parseCsvLine('a""a,b,c')).toEqual(['a""a', 'b', 'c'])
-	expect(parseCsvLine('a"a,b,c')).toEqual(['a"a', 'b', 'c'])
-	expect(parseCsvLine('a\tb\tc', '\t')).toEqual(['a', 'b', 'c'])
-	expect(parseCsvLine('a\tb,c', '\t,')).toEqual(['a', 'b', 'c'])
-	expect(parseCsvLine('#a,b,c')).toBeUndefined()
-	expect(parseCsvLine('~a,b,c', ',', '#~')).toBeUndefined()
-	expect(parseCsvLine('')).toBeUndefined()
-	expect(parseCsvLine(' ')).toEqual([' '])
+	expect(header).toHaveLength(17)
+	expect(header).toEqual(['Year', 'Month', 'Day', 'Time', 'Age of Moon', 'RA h', 'RA m', 'Ra s', 'DEC deg', 'DEC m', 'DEC s', 'Rise', 'Culm', 'Set', 'Approx', 'Observable', 'Constellation'])
+	expect(data).toHaveLength(25)
+	expect(data[0]).toHaveLength(17)
+	expect(data[0]).toEqual(['2025', 'Feb', '21', '21:00 GMT-03', '24 days', '17', '03', '29', '-27', '59', '46', '23:26', '06:24', '13:25', '-11.6', '00:05 until 05:36', 'Ophiuchus'])
+	expect(data[24]).toEqual(['2025', 'Mar', '17', '21:00 GMT-03', '18 days', '14', '16', '54', '-17', '21', '51', '19:34', '02:01', '08:35', '-12.4', '20:09 until 05:47', 'Virgo'])
 })
--- src/csv.ts
+++ src/csv.ts
@@ -1,86 +1,10 @@
-export type CsvRow = string[]
+import { parse } from 'csv-parse/sync'
 
-export interface CsvTable {
-	readonly header: CsvRow
-	readonly data: CsvRow[]
-}
+export type CsvRow = string[]
 
 export const CSV_DELIMITER = ','
 export const TSV_DELIMITER = '\t'
 
-export function readCsv(text: string | string[], delimiter: string = CSV_DELIMITER, comment: string = '#'): CsvTable | undefined {
-	let header: CsvRow = []
-	const data: CsvRow[] = []
-
-	const lines = typeof text === 'string' ? text.split('\n') : text
-
-	for (const line of lines) {
-		const item = parseCsvLine(line, delimiter, comment)
-
-		if (item) {
-			if (!header.length) {
-				header = item
-			} else {
-				data.push(item)
-			}
-		}
-	}
-
-	return { header, data }
-}
-
-const DOUBLE_QUOTE = '"'
-const IDLE = -1
-const SIMPLE_COLUMN = 0
-const QUOTED_COLUMN = 1
-const END = 2
-
-export function parseCsvLine(line: string, delimiter: string = CSV_DELIMITER, comment: string = '#') {
-	if (!line || comment.includes(line[0])) return undefined
-
-	let state = IDLE
-	let start = -1
-	let end = -1
-	const ret: string[] = []
-	const length = line.length - 1
-
-	for (let i = 0; i <= length; i++) {
-		const c = line[i]
-
-		switch (state) {
-			case IDLE: {
-				state = c === DOUBLE_QUOTE ? QUOTED_COLUMN : SIMPLE_COLUMN
-				start = i
-				end = start + 1
-				break
-			}
-			case SIMPLE_COLUMN: {
-				if (delimiter.includes(c)) {
-					state = END
-				} else {
-					end++
-				}
-
-				break
-			}
-			case QUOTED_COLUMN: {
-				if (c === DOUBLE_QUOTE) {
-					start++
-					state = SIMPLE_COLUMN
-				} else {
-					end++
-				}
-
-				break
-			}
-		}
-
-		if (state === END || i === length) {
-			ret.push(line.substring(start, end))
-			start = end = -1
-			state = IDLE
-		}
-	}
-
-	return ret
+export function readCsv(input: Buffer | string, delimiter: string | string[] = CSV_DELIMITER, comment: string = '#'): CsvRow[] {
+	return parse(input, { columns: false, comment, delimiter })
 }
