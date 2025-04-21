cdb7ca36a8e0ad21c3167beafe3e7ce6beae9b27
tiagohm <tiago.henrique.cco@gmail.com>
2025-04-21 15:52:13 -0300

    implement constellation

---
+++ tests/constellation.test.ts
@@ -0,0 +1,30 @@
+import { expect, test } from 'bun:test'
+import { deg, hour } from '../src/angle'
+import { constellation } from '../src/constellation'
+import { Timescale, timeBesselian } from '../src/time'
+
+const B1875 = timeBesselian(1875, Timescale.TT)
+const B1950 = timeBesselian(1950, Timescale.TT)
+
+test('constellation', () => {
+	expect(constellation(hour(9), deg(65), B1950)).toBe('UMA')
+	expect(constellation(hour(23.5), deg(-20), B1950)).toBe('AQR')
+	expect(constellation(hour(5.12), deg(9.12), B1950)).toBe('ORI')
+	expect(constellation(hour(9.4555), deg(-19.9), B1950)).toBe('HYA')
+	expect(constellation(hour(12.8888), deg(22), B1950)).toBe('COM')
+	expect(constellation(hour(15.6687), deg(-12.1234), B1950)).toBe('LIB')
+	expect(constellation(hour(19), deg(-40), B1950)).toBe('CRA')
+	expect(constellation(hour(6.2222), deg(-81.1234), B1950)).toBe('MEN')
+
+	expect(constellation(hour(6.241), deg(3), B1875)).toBe('ORI')
+	expect(constellation(hour(6.2416), deg(3), B1875)).toBe('ORI')
+	expect(constellation(hour(6.24166), deg(3), B1875)).toBe('ORI')
+	expect(constellation(hour(6.24171), deg(3), B1875)).toBe('MON')
+	expect(constellation(hour(22), deg(86.16), B1875)).toBe('CEP')
+	expect(constellation(hour(22), deg(86.1666), B1875)).toBe('CEP')
+	expect(constellation(hour(22), deg(86.16668), B1875)).toBe('UMI')
+	expect(constellation(hour(22), deg(86.1668), B1875)).toBe('UMI')
+
+	expect(constellation(deg(135), deg(65))).toBe('UMA')
+	expect(constellation(hour(15), deg(30))).toBe('BOO')
+})
