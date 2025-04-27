8ad21b2707bc2857f550228d83426a6cffc5ad25
tiagohm <tiago.henrique.cco@gmail.com>
2025-04-27 11:37:15 -0300

    implement phd2

--- src/phd2.ts
+++ src/phd2.ts
@@ -2,7 +2,7 @@ import type { Socket } from 'bun'
 
 export const DEFAULT_PHD2_PORT = 4400
 
-export type Phd2EventType =
+export type PHD2EventType =
 	| 'Alert'
 	| 'AppState'
 	| 'Calibrating'
@@ -38,50 +38,56 @@ export type GuideDirection = 'NORTH' | 'SOUTH' | 'WEST' | 'EAST'
 
 export type WhichMount = 'MOUNT' | 'AO' | 'BOTH'
 
-export type Phd2ConfigurationChangeEvent = Phd2Event<'ConfigurationChange'>
+export type DeclinationGuideMode = 'Off' | 'Auto' | 'North' | 'South'
 
-export type Phd2GuidingStoppedEvent = Phd2Event<'GuidingStopped'>
+export type RateUnit = 'arcsec/hr' | 'pixels/hr'
 
-export type Phd2LockPositionLostEvent = Phd2Event<'LockPositionLost'>
+export type ShiftAxis = 'RA/Dec' | 'X/Y'
 
-export type Phd2LockPositionShiftLimitReachedEvent = Phd2Event<'LockPositionShiftLimitReached'>
+export type PHD2ConfigurationChangeEvent = PHD2Event<'ConfigurationChange'>
 
-export type Phd2LoopingExposuresStoppedEvent = Phd2Event<'LoopingExposuresStopped'>
+export type PHD2GuidingStoppedEvent = PHD2Event<'GuidingStopped'>
 
-export type Phd2PausedEvent = Phd2Event<'Paused'>
+export type PHD2LockPositionLostEvent = PHD2Event<'LockPositionLost'>
 
-export type Phd2ResumedEvent = Phd2Event<'Resumed'>
+export type PHD2LockPositionShiftLimitReachedEvent = PHD2Event<'LockPositionShiftLimitReached'>
 
-export type Phd2SettleBeginEvent = Phd2Event<'SettleBegin'>
+export type PHD2LoopingExposuresStoppedEvent = PHD2Event<'LoopingExposuresStopped'>
 
-export type Phd2StartGuidingEvent = Phd2Event<'StartGuiding'>
+export type PHD2PausedEvent = PHD2Event<'Paused'>
 
-export type Phd2CommandResult<T> = { success: false; error: Phd2Error | 'timeout' } | { success: true; result: T }
+export type PHD2ResumedEvent = PHD2Event<'Resumed'>
 
-export interface Phd2Event<E extends Phd2EventType> {
+export type PHD2SettleBeginEvent = PHD2Event<'SettleBegin'>
+
+export type PHD2StartGuidingEvent = PHD2Event<'StartGuiding'>
+
+export type PHD2CommandResult<T> = { success: false; error: PHD2Error | 'timeout' } | { success: true; result: T }
+
+export interface PHD2Event<E extends PHD2EventType> {
 	readonly Event: E
 	readonly Timestamp: number
 	readonly Host: string
 	readonly Inst: number
 }
 
-export interface Phd2VersionEvent extends Phd2Event<'Version'> {
+export interface PHD2VersionEvent extends PHD2Event<'Version'> {
 	readonly PHD2Version: string
 	readonly PHD2Subver: string
 	readonly OverlapSupport: boolean
 	readonly MsgVersion: number
 }
 
-export interface Phd2AlertEvent extends Phd2Event<'Alert'> {
+export interface PHD2AlertEvent extends PHD2Event<'Alert'> {
 	readonly Msg: string
 	readonly Type: AlertType
 }
 
-export interface Phd2AppStateEvent extends Phd2Event<'AppState'> {
+export interface PHD2AppStateEvent extends PHD2Event<'AppState'> {
 	readonly State: AppState
 }
 
-export interface Phd2CalibratingEvent extends Phd2Event<'Calibrating'> {
+export interface PHD2CalibratingEvent extends PHD2Event<'Calibrating'> {
 	readonly Mount: string
 	readonly dir: string
 	readonly dist: number
@@ -92,24 +98,24 @@ export interface Phd2CalibratingEvent extends Phd2Event<'Calibrating'> {
 	readonly State: string
 }
 
-export interface Phd2CalibrationCompleteEvent extends Phd2Event<'CalibrationComplete'> {
+export interface PHD2CalibrationCompleteEvent extends PHD2Event<'CalibrationComplete'> {
 	readonly Mount: string
 }
 
-export interface Phd2CalibrationDataFlippedEvent extends Phd2Event<'CalibrationDataFlipped'> {
+export interface PHD2CalibrationDataFlippedEvent extends PHD2Event<'CalibrationDataFlipped'> {
 	readonly Mount: string
 }
 
-export interface Phd2CalibrationFailedEvent extends Phd2Event<'CalibrationFailed'> {
+export interface PHD2CalibrationFailedEvent extends PHD2Event<'CalibrationFailed'> {
 	readonly Reason: string
 }
 
-export interface Phd2GuideParamChangeEvent extends Phd2Event<'GuideParamChange'> {
+export interface PHD2GuideParamChangeEvent extends PHD2Event<'GuideParamChange'> {
 	readonly Name: string
 	readonly Value?: unknown
 }
 
-export interface Phd2GuideStepEvent extends Phd2Event<'GuideStep'> {
+export interface PHD2GuideStepEvent extends PHD2Event<'GuideStep'> {
 	readonly Frame: number
 	readonly Time: number
 	readonly Mount: string
@@ -130,35 +136,35 @@ export interface Phd2GuideStepEvent extends Phd2Event<'GuideStep'> {
 	readonly ErrorCode: number
 }
 
-export interface Phd2GuidingDitheredEvent extends Phd2Event<'GuidingDithered'> {
+export interface PHD2GuidingDitheredEvent extends PHD2Event<'GuidingDithered'> {
 	readonly dx: number
 	readonly dy: number
 }
 
-export interface Phd2LockPositionSetEvent extends Phd2Event<'LockPositionSet'> {
+export interface PHD2LockPositionSetEvent extends PHD2Event<'LockPositionSet'> {
 	readonly X: number
 	readonly Y: number
 }
 
-export interface Phd2LoopingExposuresEvent extends Phd2Event<'LoopingExposures'> {
+export interface PHD2LoopingExposuresEvent extends PHD2Event<'LoopingExposures'> {
 	readonly Frame: number
 }
 
-export interface Phd2SettleDoneEvent extends Phd2Event<'SettleDone'> {
+export interface PHD2SettleDoneEvent extends PHD2Event<'SettleDone'> {
 	readonly Status: number
 	readonly TotalFrames: number
 	readonly DroppedFrames: number
 	readonly Error?: string
 }
 
-export interface Phd2SettlingEvent extends Phd2Event<'Settling'> {
+export interface PHD2SettlingEvent extends PHD2Event<'Settling'> {
 	readonly Distance: number
 	readonly Time: number
 	readonly SettleTime: number
 	readonly StarLocked: boolean
 }
 
-export interface Phd2StarLostEvent extends Phd2Event<'StarLost'> {
+export interface PHD2StarLostEvent extends PHD2Event<'StarLost'> {
 	readonly Frame: number
 	readonly Time: number
 	readonly StarMass: number
@@ -168,92 +174,154 @@ export interface Phd2StarLostEvent extends Phd2Event<'StarLost'> {
 	readonly Status: string
 }
 
-export interface Phd2StarSelectedEvent extends Phd2Event<'StarSelected'> {
+export interface PHD2StarSelectedEvent extends PHD2Event<'StarSelected'> {
 	readonly X: number
 	readonly Y: number
 }
 
-export interface Phd2StartCalibrationEvent extends Phd2Event<'StartCalibration'> {
+export interface PHD2StartCalibrationEvent extends PHD2Event<'StartCalibration'> {
 	readonly Mount: string
 }
 
-export interface Phd2Error {
+export interface PHD2Error {
 	readonly code: number
 	readonly message: string
 }
 
-export interface Phd2JsonRpcEvent {
+export interface PHD2JsonRpcEvent {
 	readonly jsonrpc: string
 	readonly id: string
 	readonly result: unknown
-	readonly error?: Phd2Error
+	readonly error?: PHD2Error
 }
 
-export type Phd2Events =
-	| Phd2AlertEvent
-	| Phd2AppStateEvent
-	| Phd2CalibratingEvent
-	| Phd2CalibrationCompleteEvent
-	| Phd2CalibrationDataFlippedEvent
-	| Phd2CalibrationFailedEvent
-	| Phd2ConfigurationChangeEvent
-	| Phd2GuideParamChangeEvent
-	| Phd2GuideStepEvent
-	| Phd2GuidingDitheredEvent
-	| Phd2GuidingStoppedEvent
-	| Phd2LockPositionLostEvent
-	| Phd2LockPositionSetEvent
-	| Phd2LockPositionShiftLimitReachedEvent
-	| Phd2LoopingExposuresEvent
-	| Phd2LoopingExposuresStoppedEvent
-	| Phd2PausedEvent
-	| Phd2ResumedEvent
-	| Phd2SettleBeginEvent
-	| Phd2SettleDoneEvent
-	| Phd2SettlingEvent
-	| Phd2StarLostEvent
-	| Phd2StarSelectedEvent
-	| Phd2StartCalibrationEvent
-	| Phd2StartGuidingEvent
-	| Phd2VersionEvent
-	| Phd2JsonRpcEvent
+export type PHD2Events =
+	| PHD2AlertEvent
+	| PHD2AppStateEvent
+	| PHD2CalibratingEvent
+	| PHD2CalibrationCompleteEvent
+	| PHD2CalibrationDataFlippedEvent
+	| PHD2CalibrationFailedEvent
+	| PHD2ConfigurationChangeEvent
+	| PHD2GuideParamChangeEvent
+	| PHD2GuideStepEvent
+	| PHD2GuidingDitheredEvent
+	| PHD2GuidingStoppedEvent
+	| PHD2LockPositionLostEvent
+	| PHD2LockPositionSetEvent
+	| PHD2LockPositionShiftLimitReachedEvent
+	| PHD2LoopingExposuresEvent
+	| PHD2LoopingExposuresStoppedEvent
+	| PHD2PausedEvent
+	| PHD2ResumedEvent
+	| PHD2SettleBeginEvent
+	| PHD2SettleDoneEvent
+	| PHD2SettlingEvent
+	| PHD2StarLostEvent
+	| PHD2StarSelectedEvent
+	| PHD2StartCalibrationEvent
+	| PHD2StartGuidingEvent
+	| PHD2VersionEvent
+	| PHD2JsonRpcEvent
 
-export interface Phd2Command {
+export interface PHD2Command {
 	readonly id: string
 	readonly method: string
 	readonly params?: Record<string, unknown> | unknown[]
 }
 
-export interface Phd2Device {
+export interface PHD2Device {
 	readonly name: string
 	readonly connected: boolean
 }
 
-export interface Phd2Equipment {
-	readonly camera: Phd2Device
-	readonly mount: Phd2Device
-	readonly aux_mount: Phd2Device
-	readonly AO: Phd2Device
-	readonly rotator: Phd2Device
+export interface PHD2Equipment {
+	readonly camera?: PHD2Device
+	readonly mount?: PHD2Device
+	readonly aux_mount?: PHD2Device
+	readonly AO?: PHD2Device
+	readonly rotator?: PHD2Device
 }
 
-export interface Phd2ClientOptions {
-	handler?: Phd2ClientHandler
+export interface PHD2StarPos {
+	readonly x: number
+	readonly y: number
 }
 
-export interface Phd2ClientHandler {
-	event?: (client: Phd2Client, event: Exclude<Phd2Events, Phd2JsonRpcEvent>) => void
-	command?: (client: Phd2Client, command: Phd2Command, success: boolean, result: Phd2Error | unknown) => void
+export interface PHD2Roi {
+	x: number
+	y: number
+	width: number
+	height: number
+}
+
+export interface PHD2DitherSettle {
+	pixels: number
+	time: number
+	timeout: number
+}
+
+export interface PHD2CalibrationData {
+	readonly calibrated: boolean
+	readonly xAngle: number
+	readonly xRate: number
+	readonly xParity: '+' | '-'
+	readonly yAngle: number
+	readonly yRate: number
+	readonly yParity: '+' | '-'
+}
+
+export interface PHD2LockShiftParams {
+	readonly enabled: boolean
+	readonly rate: readonly [number, number]
+	readonly units: RateUnit
+	readonly axes: ShiftAxis
+}
+
+export interface PHD2Profile {
+	readonly id: number
+	readonly name: string
+	readonly selected: boolean
+}
+
+export interface PHD2StarImage {
+	readonly frame: number
+	readonly width: number
+	readonly height: number
+	readonly star_pos: PHD2StarPos
+	readonly pixels: string
+}
+
+export interface PHD2ClientOptions {
+	handler?: PHD2ClientHandler
+}
+
+export interface PHD2ClientHandler {
+	event?: (client: PHD2Client, event: Exclude<PHD2Events, PHD2JsonRpcEvent>) => void
+	command?: (client: PHD2Client, command: PHD2Command, success: boolean, result: PHD2Error | unknown) => void
+}
+
+const DEFAULT_PHD2_SUBFRAME: PHD2Roi = {
+	x: 0,
+	y: 0,
+	width: 0,
+	height: 0,
+}
+
+const DEFAULT_PHD2_DITHER_SETTLE: PHD2DitherSettle = {
+	pixels: 1.5, // px
+	time: 10, // s
+	timeout: 30, // s
 }
 
 const CLRF = Buffer.from([13, 10])
 
-export class Phd2Client {
+export class PHD2Client {
 	// biome-ignore lint/suspicious/noExplicitAny:
-	private readonly commands = new Map<string, { promise: PromiseWithResolvers<Phd2CommandResult<any>>; timer: any; command: Phd2Command }>()
+	private readonly commands = new Map<string, { promise: PromiseWithResolvers<PHD2CommandResult<any>>; timer: any; command: PHD2Command }>()
 	private socket?: Socket
 
-	constructor(private readonly options?: Phd2ClientOptions) {}
+	constructor(private readonly options?: PHD2ClientOptions) {}
 
 	async connect(hostname: string, port: number = DEFAULT_PHD2_PORT) {
 		if (this.socket) return
@@ -283,8 +351,8 @@ export class Phd2Client {
 		if (!this.socket) return undefined
 
 		const id = Bun.randomUUIDv7()
-		const command: Phd2Command = { method, params, id }
-		let promise: PromiseWithResolvers<Phd2CommandResult<T>> | undefined
+		const command: PHD2Command = { method, params, id }
+		let promise: PromiseWithResolvers<PHD2CommandResult<T>> | undefined
 
 		if (timeout) {
 			promise = Promise.withResolvers()
@@ -298,13 +366,15 @@ export class Phd2Client {
 		return promise?.promise
 	}
 
-	findStar(x: number = 0, y: number = 0, width: number = 0, height: number = 0) {
-		const subframe = x !== undefined && y !== undefined && width && height ? [x, y, width, height] : undefined
+	findStar(roi?: Partial<PHD2Roi>) {
+		const { x, y, width, height } = mergeInto(roi, DEFAULT_PHD2_SUBFRAME)
+		const subframe = width && height ? [x, y, width, height] : undefined
 		return this.send<[number, number]>('find_star', subframe)
 	}
 
-	startCapture(exposure: number, x: number = 0, y: number = 0, width: number = 0, height: number = 0) {
-		const subframe = x !== undefined && y !== undefined && width && height ? [x, y, width, height] : undefined
+	startCapture(exposure: number, roi?: Partial<PHD2Roi>) {
+		const { x, y, width, height } = mergeInto(roi, DEFAULT_PHD2_SUBFRAME)
+		const subframe = width && height ? [x, y, width, height] : undefined
 		return this.send<number>('capture_single_frame', { exposure, subframe })
 	}
 
@@ -318,6 +388,111 @@ export class Phd2Client {
 
 	deselectStar() {
 		return this.send<number>('deselect_star')
+	}
+
+	dither(amount: number, raOnly: boolean = false, settle?: Partial<PHD2DitherSettle>) {
+		settle = mergeInto(settle, DEFAULT_PHD2_DITHER_SETTLE)
+		return this.send<number>('shutdown', { amount, raOnly, settle })
+	}
+
+	flipCalibration() {
+		return this.send<number>('flip_calibration')
+	}
+
+	getAlgorithmParam(axis: 'RA' | 'DEC', name: string) {
+		return this.send<unknown>('get_algo_param', { axis, name })
+	}
+
+	getAlgorithmParamNames(axis: 'RA' | 'DEC') {
+		return this.send<string[]>('get_algo_param_names', { axis })
+	}
+
+	getAppState() {
+		return this.send<AppState>('get_app_state')
+	}
+
+	getCalibrated() {
+		return this.send<boolean>('get_calibrated')
+	}
+
+	getCalibrationData(which: WhichMount) {
+		return this.send<PHD2CalibrationData>('get_calibration_data', [which])
+	}
+
+	getCameraBinning() {
+		return this.send<number>('get_camera_binning')
+	}
+
+	getCameraFrameSize() {
+		return this.send<[number, number]>('get_camera_frame_size')
+	}
+
+	getConnected() {
+		return this.send<boolean>('get_connected')
+	}
+
+	getCurrentEquipment() {
+		return this.send<PHD2Equipment>('get_current_equipment')
+	}
+
+	getDeclinationGuideMode() {
+		return this.send<DeclinationGuideMode>('get_dec_guide_mode')
+	}
+
+	getExposure() {
+		return this.send<number>('get_exposure')
+	}
+
+	getExposureDurations() {
+		return this.send<number[]>('get_exposure_durations')
+	}
+
+	getGuideOutputEnabled() {
+		return this.send<boolean>('get_guide_output_enabled')
+	}
+
+	getLockPosition() {
+		return this.send<[number, number] | null>('get_lock_position')
+	}
+
+	getLockShiftEnabled() {
+		return this.send<boolean>('get_lock_shift_enabled')
+	}
+
+	getLockShiftParams() {
+		return this.send<PHD2LockShiftParams>('get_lock_shift_params')
+	}
+
+	getPaused() {
+		return this.send<boolean>('get_paused')
+	}
+
+	getPixelScale() {
+		return this.send<number>('get_pixel_scale')
+	}
+
+	getProfile() {
+		return this.send<Omit<PHD2Profile, 'selected'>>('get_profile')
+	}
+
+	getProfiles() {
+		return this.send<PHD2Profile[]>('get_profiles')
+	}
+
+	getSearchRegion() {
+		return this.send<number>('get_search_region')
+	}
+
+	getSettling() {
+		return this.send<boolean>('get_settling')
+	}
+
+	getStarImage() {
+		return this.send<PHD2StarImage>('get_star_image')
+	}
+
+	getUseSubframes() {
+		return this.send<boolean>('get_use_subframes')
 	}
 
 	shutdown() {
@@ -341,7 +516,7 @@ export class Phd2Client {
 	}
 
 	private processEvent(data: string) {
-		const event = JSON.parse(data) as Phd2Events
+		const event = JSON.parse(data) as PHD2Events
 
 		if ('jsonrpc' in event) {
 			const { id, error, result } = event
@@ -363,4 +538,15 @@ export class Phd2Client {
 			this.options!.handler!.event?.(this, event)
 		}
 	}
+}
+
+function mergeInto<T extends {}>(target: Partial<T> | undefined | null, source: T) {
+	if (!target || target === source) return source
+
+	for (const key in source) {
+		if (target[key] !== undefined) continue
+		target[key] = source[key]
+	}
+
+	return target as T
 }
--- tests/phd2.test.ts
+++ tests/phd2.test.ts
@@ -1,11 +1,11 @@
 import { test } from 'bun:test'
-import { Phd2Client } from '../src/phd2'
+import { PHD2Client } from '../src/phd2'
 
 test.skip('client', async () => {
-	const client = new Phd2Client({
+	const client = new PHD2Client({
 		handler: {
 			event: (_, event) => {
-				console.info(event)
+				console.info('EVENT:', event)
 			},
 		},
 	})
@@ -14,5 +14,31 @@ test.skip('client', async () => {
 
 	await Bun.sleep(1000)
 
-	await client.findStar()
+	console.info('FIND_STAR:', await client.findStar())
+	console.info('GET_ALGORITHM_PARAM_NAMES(RA):', await client.getAlgorithmParamNames('RA'))
+	console.info('GET_ALGORITHM_PARAM_NAMES(DEC):', await client.getAlgorithmParamNames('DEC'))
+	console.info('GET_ALGORITHM_PARAM(RA, NAME):', await client.getAlgorithmParam('RA', 'algorithmName'))
+	console.info('GET_ALGORITHM_PARAM(DEC, NAME):', await client.getAlgorithmParam('DEC', 'algorithmName'))
+	console.info('GET_APP_STATE:', await client.getAppState())
+	console.info('GET_CALIBRATED:', await client.getCalibrated())
+	console.info('GET_CALIBRATION_DATA(MOUNT):', await client.getCalibrationData('MOUNT'))
+	console.info('GET_CAMERA_BINNING:', await client.getCameraBinning())
+	console.info('GET_CAMERA_FRAME_SIZE:', await client.getCameraFrameSize())
+	console.info('GET_CONNECTED:', await client.getConnected())
+	console.info('GET_CURRENT_EQUIPMENT:', await client.getCurrentEquipment())
+	console.info('GET_DECLINATION_GUIDE_MODE:', await client.getDeclinationGuideMode())
+	console.info('GET_EXPOSURE:', await client.getExposure())
+	console.info('GET_EXPOSURE_DURATIONS:', await client.getExposureDurations())
+	console.info('GET_GUIDE_OUTPUT_ENABLED:', await client.getGuideOutputEnabled())
+	console.info('GET_LOCK_POSITION:', await client.getLockPosition())
+	console.info('GET_LOCK_SHIFT_ENABLED:', await client.getLockShiftEnabled())
+	console.info('GET_LOCK_SHIFT_PARAMS:', await client.getLockShiftParams())
+	console.info('GET_PAUSED:', await client.getPaused())
+	console.info('GET_PIXEL_SCALE:', await client.getPixelScale())
+	console.info('GET_PROFILE:', await client.getProfile())
+	console.info('GET_PROFILES:', await client.getProfiles())
+	console.info('GET_SEARCH_REGION:', await client.getSearchRegion())
+	console.info('GET_SETTLING:', await client.getSettling())
+	console.info('GET_STAR_IMAGE:', await client.getStarImage())
+	console.info('GET_USE_SUBFRAMES:', await client.getUseSubframes())
 }, 5000)
