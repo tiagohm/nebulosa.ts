68df1864f9389c9f3dd01fc842728c2f9cb001b3
tiagohm <tiago.henrique.cco@gmail.com>
2025-05-12 11:01:27 -0300

    implement image

--- src/image.ts
+++ src/image.ts
@@ -1,6 +1,7 @@
 import sharp, { type AvifOptions, type FormatEnum, type GifOptions, type HeifOptions, type Jp2Options, type JpegOptions, type JxlOptions, type OutputInfo, type OutputOptions, type PngOptions, type TiffOptions, type WebpOptions } from 'sharp'
 import { Bitpix, type Fits, type FitsData, type FitsHdu, type FitsHeader, bitpix, bitpixInBytes, height, numberOfChannels, text, width, writeFits } from './fits'
 import { type Sink, type Source, bufferSink, bufferSource, readUntil } from './io'
+import { Histogram } from './statistics'
 
 export type ImageChannel = 'RED' | 'GREEN' | 'BLUE'
 
@@ -11,6 +12,14 @@ export type CfaPattern = 'RGGB' | 'BGGR' | 'GBRG' | 'GRBG' | 'GRGB' | 'GBGR' | '
 export type SCNRProtectionMethod = 'MAXIMUM_MASK' | 'ADDITIVE_MASK' | 'AVERAGE_NEUTRAL' | 'MAXIMUM_NEUTRAL' | 'MINIMUM_NEUTRAL'
 
 export type SCNRAlgorithm = (a: number, b: number, c: number, amount: number) => number
+
+export type Grayscale = Readonly<Record<Lowercase<ImageChannel>, number>>
+
+export type GrayscaleAlgorithm = 'BT709' | 'RMY' | 'Y' | Grayscale
+
+export type ImageChannelOrGray = ImageChannel | GrayscaleAlgorithm | 'GRAY'
+
+export type HistogramPixelTransform = (p: number) => number
 
 export interface WriteImageToFormatOptions {
 	format: OutputOptions | JpegOptions | PngOptions | WebpOptions | AvifOptions | HeifOptions | JxlOptions | GifOptions | Jp2Options | TiffOptions
@@ -34,8 +43,24 @@ export interface ImageMetadata {
 	readonly bayer?: CfaPattern
 }
 
+export const BT709_GRAYSCALE: Grayscale = { red: 0.2125, green: 0.7154, blue: 0.0721 }
+export const RMY_GRAYSCALE: Grayscale = { red: 0.5, green: 0.419, blue: 0.081 }
+export const Y_GRAYSCALE: Grayscale = { red: 0.299, green: 0.587, blue: 0.114 }
+export const RED_GRAYSCALE: Grayscale = { red: 1, green: 0, blue: 0 }
+export const GREEN_GRAYSCALE: Grayscale = { red: 0, green: 1, blue: 0 }
+export const BLUE_GRAYSCALE: Grayscale = { red: 0, green: 0, blue: 1 }
+export const DEFAULT_GRAYSCALE = BT709_GRAYSCALE
+
 export function cfaPattern(header: FitsHeader) {
 	return text(header, 'BAYERPAT') as CfaPattern | undefined
+}
+
+export function channelIndex(channel?: ImageChannelOrGray) {
+	return channel === 'GREEN' ? 1 : channel === 'BLUE' ? 2 : 0
+}
+
+export function grayscaleFromChannel(channel?: ImageChannelOrGray): Grayscale {
+	return channel === 'BT709' ? BT709_GRAYSCALE : channel === 'RMY' ? RMY_GRAYSCALE : channel === 'Y' ? Y_GRAYSCALE : channel === 'GRAY' ? DEFAULT_GRAYSCALE : channel === 'RED' ? RED_GRAYSCALE : channel === 'GREEN' ? GREEN_GRAYSCALE : channel === 'BLUE' ? BLUE_GRAYSCALE : (channel ?? DEFAULT_GRAYSCALE)
 }
 
 export async function readImageFromFits(fitsOrHdu?: Fits | FitsHdu): Promise<Image | undefined> {
@@ -167,37 +192,57 @@ export function writeImageToFits(image: Image, output: Buffer | Sink) {
 	return writeFits(output, [hdu])
 }
 
+function truncatePixel(p: number) {
+	return Math.max(0, Math.min(Math.trunc(p * 65535), 65535))
+}
+
 // Apply Screen Transfer Function to image.
 // https://pixinsight.com/doc/docs/XISF-1.0-spec/XISF-1.0-spec.html#__XISF_Data_Objects_:_XISF_Image_:_Display_Function__
 // https://pixinsight.com/tutorials/24-bit-stf/
-export function stf(image: Image, midtone: number = 0.5, shadow: number = 0, highlight: number = 1) {
+export function stf(image: Image, midtone: number = 0.5, shadow: number = 0, highlight: number = 1, channel?: ImageChannelOrGray) {
 	if (midtone === 0.5 && shadow === 0 && highlight === 1) return image
 
-	const rangeFactor = shadow === highlight ? 1 : 1 / (highlight - shadow)
-	const k1 = (midtone - 1) * rangeFactor
-	const k2 = (2 * midtone - 1) * rangeFactor
+	const factor = shadow === highlight ? 1 : 1 / (highlight - shadow)
+	const k1 = (midtone - 1) * factor
+	const k2 = (2 * midtone - 1) * factor
 	const lut = new Float64Array(65536).fill(NaN)
 
-	function df(value: number) {
-		const p = Math.max(0, Math.min(Math.trunc(value * 65535), 65535))
-		if (!Number.isNaN(lut[p])) return lut[p]
-		if (value < shadow) return 0
-		if (value > highlight) return 1
+	const { raw, metadata } = image
 
-		const i = value - shadow
-		value = (i * k1) / (i * k2 - midtone)
-		lut[p] = value
+	const s = metadata.channels === 3 ? channelIndex(channel) : 0
+	const p = metadata.channels === 3 && channel ? 3 : 1
 
-		return value
-	}
+	for (let i = s; i < raw.length; i += p) {
+		let value = raw[i]
+		const p = truncatePixel(value)
 
-	const { raw } = image
-
-	for (let i = 0; i < raw.length; i++) {
-		raw[i] = df(raw[i])
+		if (!Number.isNaN(lut[p])) raw[i] = lut[p]
+		else if (value < shadow) raw[i] = 0
+		else if (value > highlight) raw[i] = 1
+		else {
+			const d = value - shadow
+			value = (d * k1) / (d * k2 - midtone)
+			lut[p] = value
+			raw[i] = value
+		}
 	}
 
 	return image
+}
+
+export const DEFAULT_MEAN_BACKGROUND = 0.25
+export const DEFAULT_CLIPPING_POINT = -2.8
+
+export function autoStf(image: Image, channel?: ImageChannelOrGray, meanBackground: number = DEFAULT_MEAN_BACKGROUND, clippingPoint: number = DEFAULT_CLIPPING_POINT) {
+	const med = median(image, channel)
+	const mad = medianAbsoluteDiviation(image, channel, true, med)
+	const upperHalf = med > 0.5
+	const shadow = upperHalf || mad === 0 ? 0 : Math.min(1, Math.max(0, med + clippingPoint * mad))
+	const highlight = !upperHalf || mad === 0 ? 1 : Math.min(1, Math.max(0, med - clippingPoint * mad))
+	const x = upperHalf ? meanBackground : med - shadow
+	const m = upperHalf ? highlight - med : meanBackground
+	const midtone = x === 0 ? 0 : x === m ? 0.5 : x === 1 ? 1 : ((m - 1) * x) / ((2 * m - 1) * x - m)
+	return { shadow, midtone, highlight } as const
 }
 
 export function scnrMaximumMask(a: number, b: number, c: number, amount: number) {
@@ -234,8 +279,8 @@ const SCNR_ALGORITHMS: Readonly<Record<SCNRProtectionMethod, SCNRAlgorithm>> = {
 }
 
 // Subtractive Chromatic Noise Reduction
-export function scnr(image: Image, channel: ImageChannel | 'GRAY' = 'GREEN', amount: number = 0.5, method: SCNRProtectionMethod = 'MAXIMUM_MASK') {
-	if (image.metadata.channels === 3 && channel !== 'GRAY') {
+export function scnr(image: Image, channel: ImageChannel = 'GREEN', amount: number = 0.5, method: SCNRProtectionMethod = 'MAXIMUM_MASK') {
+	if (image.metadata.channels === 3) {
 		const p0 = channel === 'RED' ? 0 : channel === 'GREEN' ? 1 : 2
 		const p1 = channel === 'RED' ? 1 : channel === 'GREEN' ? 2 : 0
 		const p2 = channel === 'RED' ? 2 : channel === 'GREEN' ? 0 : 1
@@ -258,20 +303,20 @@ export function scnr(image: Image, channel: ImageChannel | 'GRAY' = 'GREEN', amo
 export function horizontalFlip(image: Image) {
 	const { raw, metadata } = image
 	const { height, channels, strideInPixels } = metadata
+	const maxW = Math.trunc(strideInPixels / 2)
+	const sc = strideInPixels - channels
 
 	for (let y = 0; y < height; y++) {
 		const k = y * strideInPixels
 
-		for (let x = 0; x < strideInPixels / 2; x += channels) {
-			const sx = strideInPixels - x - channels
+		for (let x = 0; x < maxW; x += channels) {
+			let si = k + sc - x
+			let ei = k + x
 
-			const si = k + sx
-			const ei = k + x
-
-			for (let i = 0; i < channels; i++) {
-				const p = raw[si + i]
-				raw[si + i] = raw[ei + i]
-				raw[ei + i] = p
+			for (let i = 0; i < channels; i++, si++, ei++) {
+				const p = raw[si]
+				raw[si] = raw[ei]
+				raw[ei] = p
 			}
 		}
 	}
@@ -283,22 +328,66 @@ export function verticalFlip(image: Image) {
 	const { raw, metadata } = image
 	const { height, channels, strideInPixels } = metadata
 	const sh = (height - 1) * strideInPixels
+	const maxH = Math.trunc(height / 2)
 
-	for (let y = 0; y < height / 2; y++) {
+	for (let y = 0; y < maxH; y++) {
 		const k = y * strideInPixels
 		const ek = sh - k
 
 		for (let x = 0; x < strideInPixels; x += channels) {
-			const si = k + x
-			const ei = ek + x
+			let si = k + x
+			let ei = ek + x
 
-			for (let i = 0; i < channels; i++) {
-				const p = raw[si + i]
-				raw[si + i] = raw[ei + i]
-				raw[ei + i] = p
+			for (let i = 0; i < channels; i++, si++, ei++) {
+				const p = raw[si]
+				raw[si] = raw[ei]
+				raw[ei] = p
 			}
 		}
 	}
 
 	return image
+}
+
+export function histogram(image: Image, channel?: ImageChannelOrGray, transform?: HistogramPixelTransform) {
+	const histogram = new Int32Array(65536)
+	const { raw, metadata } = image
+
+	if (metadata.channels === 3) {
+		if (channel === 'RED' || channel === 'GREEN' || channel === 'BLUE') {
+			for (let i = channelIndex(channel); i < raw.length; i += 3) {
+				const v = raw[i]
+				const p = truncatePixel(transform?.(v) ?? v)
+				histogram[p]++
+			}
+		} else {
+			const { red, green, blue } = grayscaleFromChannel(channel)
+
+			for (let i = 0; i < raw.length; i += 3) {
+				const v = raw[i] * red + raw[i + 1] * green + raw[i + 2] * blue
+				const p = truncatePixel(transform?.(v) ?? v)
+				histogram[p]++
+			}
+		}
+	} else {
+		for (let i = 0; i < raw.length; i++) {
+			const v = raw[i]
+			const p = truncatePixel(transform?.(v) ?? v)
+			histogram[p]++
+		}
+	}
+
+	return new Histogram(histogram)
+}
+
+export function median(image: Image, channel?: ImageChannelOrGray, transform?: HistogramPixelTransform) {
+	return histogram(image, channel, transform).median() / 65535
+}
+
+const STANDARD_DEVIATION_SCALE = 1.482602218505602
+
+export function medianAbsoluteDiviation(image: Image, channel?: ImageChannelOrGray, normalized: boolean = false, m?: number) {
+	m ||= median(image, channel)
+	const mad = median(image, channel, (p) => Math.abs(p - m))
+	return normalized ? STANDARD_DEVIATION_SCALE * mad : mad
 }
