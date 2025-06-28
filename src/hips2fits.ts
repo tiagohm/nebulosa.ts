b117b8edf981d3e64ba1b8da3b318b70e31a723a
tiagohm <tiago.henrique.cco@gmail.com>
2025-06-28 10:04:46 -0300

    improve hips2fits

--- src/hips2fits.ts
+++ src/hips2fits.ts
@@ -18,17 +18,7 @@ export interface Hips2FitsOptions {
 	projection?: ProjectionType
 	coordSystem?: CoordinateFrameType
 	format?: ImageFormatType
-}
-
-const DEFAULT_HIPS_TO_FITS_OPTIONS: Required<Hips2FitsOptions> = {
-	baseUrl: HIPS2FITS_BASE_URL,
-	width: 1200,
-	height: 900,
-	rotation: 0,
-	fov: DEG2RAD,
-	projection: 'TAN',
-	coordSystem: 'icrs',
-	format: 'fits',
+	timeout?: number
 }
 
 export interface HipsSurvey {
@@ -43,15 +33,16 @@ export interface HipsSurvey {
 
 // Extracts a FITS image from a HiPS given the output image pixel size,
 // the center of projection, the type of projection and the field of view.
-export async function hips2Fits(id: string, ra: Angle, dec: Angle, { width = 1200, height = 900, rotation = 0, fov = DEG2RAD, projection = 'TAN', coordSystem = 'icrs', format = 'fits', baseUrl = '' }: Hips2FitsOptions) {
+export function hips2Fits(id: string, ra: Angle, dec: Angle, { width = 1200, height = 900, rotation = 0, fov = DEG2RAD, projection = 'TAN', coordSystem = 'icrs', format = 'fits', baseUrl = '', timeout = 60000 }: Hips2FitsOptions) {
 	const uri = `${baseUrl || HIPS2FITS_BASE_URL}hips-image-services/hips2fits?hips=${id}&ra=${toDeg(ra)}&dec=${toDeg(dec)}&width=${width}&height=${height}&projection=${projection}&fov=${toDeg(fov)}&coordsys=${coordSystem}&rotation_angle=${toDeg(rotation)}&format=${format}`
-	return fetch(uri).then((res) => res.blob())
+	const signal = AbortSignal.timeout(timeout)
+	return fetch(uri, { signal }).then((res) => res.blob())
 }
 
 // Fetches HiPS surveys with a minimum sky fraction.
 export async function hipsSurveys(minSkyFraction: number = 0.99, baseUrl?: string) {
 	const expr = encodeURIComponent(`ID=CDS* && hips_service_url*=*alasky* && dataproduct_type=image && moc_sky_fraction >= ${minSkyFraction} && obs_regime=Optical,Infrared,UV,Radio,X-ray,Gamma-ray`)
-	const uri = `${baseUrl || DEFAULT_HIPS_TO_FITS_OPTIONS.baseUrl}MocServer/query?get=record&fmt=json&expr=${expr}`
+	const uri = `${baseUrl || HIPS2FITS_BASE_URL}MocServer/query?get=record&fmt=json&expr=${expr}`
 	const response = await fetch(uri)
 	return ((await response.json()) as Record<string, unknown>[]).map(mapHipsSurvey)
 }
